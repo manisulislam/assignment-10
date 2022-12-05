e7259edb9967ae7928f9d6a1cd1d98f03cf596c8
anisul islam <anisulislam980340@gmail.com>
2022-12-05 22:21:18 +0600

    setup webste name and title

--- src/shared/Header/Header.js
+++ src/shared/Header/Header.js
@@ -6,7 +6,8 @@ const Header = () => {
         <div>
             <div className="navbar bg-base-100">
   <div className="flex-1">
-    <Link to='/' className="btn btn-ghost normal-case text-xl">daisyUI</Link>
+    <img src="" alt="" />
+    <Link to='/' className="btn btn-ghost normal-case text-xl">LEARN ACADEMY</Link>
   </div>
   <div className="flex-none">
     <ul className="menu menu-horizontal p-0">
