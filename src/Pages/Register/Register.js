3547dc9151f68b53c787a2427c906c0457bf7092
anisul islam <anisulislam980340@gmail.com>
2022-12-05 23:51:00 +0600

    setup login and register

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,10 +1,50 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
-        <div>
-            <h4>this is register</h4>
+        <div className="hero min-h-screen bg-base-200">
+  <div className="hero-content flex-col ">
+    <div className="text-center lg:text-left">
+      <h1 className="text-5xl font-bold">Register now!</h1>
+      
+    </div>
+    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+      <div className="card-body">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Full Name</span>
+          </label>
+          <input type="text" placeholder="Enter Your Name" className="input input-bordered" />
         </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Photourl</span>
+          </label>
+          <input type="text" placeholder="Enter Your Photo url" className="input input-bordered" />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input type="text" placeholder="email" className="input input-bordered" />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input type="text" placeholder="password" className="input input-bordered" />
+          <label className="label">
+            <a href="#" className="label-text-alt link link-hover">Already an accout? <Link to='/login'><span className='text-orange-500'>LogIn</span></Link></a>
+          </label>
+        </div>
+        <div className="form-control mt-6">
+          <button className="btn btn-primary">Register</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
     );
 };
 
