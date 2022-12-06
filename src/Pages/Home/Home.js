c40e601fc27191c82248fafd7861d1cad6d78e19
anisul islam <anisulislam980340@gmail.com>
2022-12-06 08:24:36 +0600

    setu hero in the home . and this is optinal

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import Header from '../../shared/Header/Header';
+
 
 const Home = () => {
     return (
-        <div>
-           
-        </div>
+        <div className="hero min-h-screen bg-base-200">
+  <div className="hero-content text-center">
+    <div className="max-w-md">
+      <h1 className="text-5xl font-bold">WELCOME TO LEARN ACADEMY</h1>
+      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      
+    </div>
+  </div>
+</div>
     );
 };
 
