e1607858bd1c0ed5e374d0e3807635be74231a8d
anisul islam <anisulislam980340@gmail.com>
2022-12-06 06:49:33 +0600

    get data from register form

--- src/Pages/LogIn/LogIn.js
+++ src/Pages/LogIn/LogIn.js
@@ -2,6 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const LogIn = () => {
+
+
+
+
+
+    const handelLogIn = event =>{
+        event.peventDefault()
+       console.log(event)
+
+    }
+
+
+
+
     return (
         <div className="hero min-h-screen bg-base-200">
   <div className="hero-content flex-col ">
@@ -10,18 +24,18 @@ const LogIn = () => {
       
     </div>
     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-      <div className="card-body">
+      <form onSubmit={handelLogIn} className="card-body">
         <div className="form-control">
           <label className="label">
             <span className="label-text">Email</span>
           </label>
-          <input type="text" placeholder="email" className="input input-bordered" />
+          <input type="text" name='email' placeholder="email" className="input input-bordered" />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Password</span>
           </label>
-          <input type="text" placeholder="password" className="input input-bordered" />
+          <input type="text" name='password' placeholder="password" className="input input-bordered" />
           <label className="label">
             <a href="#" className="label-text-alt link link-hover">Have a new account? <Link to='/register'> <span className='text-orange-500'>Register </span></Link></a>
           </label>
@@ -29,7 +43,7 @@ const LogIn = () => {
         <div className="form-control mt-6">
           <button className="btn btn-primary">Login</button>
         </div>
-      </div>
+      </form>
     </div>
   </div>
 </div>
--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -2,6 +2,21 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Register = () => {
+
+
+
+
+  const handleRegister = event =>{
+    event.preventDefault()
+    const form= event.target
+    const name = form.name.value;
+    const photo = form.photo.value;
+    const email = form.email.value;
+    const password = form.password.value;
+    form.reset()
+
+    console.log(name, photo, email, password)
+  }
     return (
         <div className="hero min-h-screen bg-base-200">
   <div className="hero-content flex-col ">
@@ -10,30 +25,30 @@ const Register = () => {
       
     </div>
     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-      <div className="card-body">
+      <form onSubmit={handleRegister} className="card-body">
         <div className="form-control">
           <label className="label">
             <span className="label-text">Full Name</span>
           </label>
-          <input type="text" placeholder="Enter Your Name" className="input input-bordered" />
+          <input type="text" name='name' placeholder="Enter Your Name" className="input input-bordered" />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Photourl</span>
           </label>
-          <input type="text" placeholder="Enter Your Photo url" className="input input-bordered" />
+          <input type="text" name='photo' placeholder="Enter Your Photo url" className="input input-bordered" />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Email</span>
           </label>
-          <input type="text" placeholder="email" className="input input-bordered" />
+          <input type="text" name='email' placeholder="email" className="input input-bordered" />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Password</span>
           </label>
-          <input type="text" placeholder="password" className="input input-bordered" />
+          <input type="text" name='password' placeholder="password" className="input input-bordered" />
           <label className="label">
             <a href="#" className="label-text-alt link link-hover">Already an accout? <Link to='/login'><span className='text-orange-500'>LogIn</span></Link></a>
           </label>
@@ -41,7 +56,7 @@ const Register = () => {
         <div className="form-control mt-6">
           <button className="btn btn-primary">Register</button>
         </div>
-      </div>
+      </form>
     </div>
   </div>
 </div>
